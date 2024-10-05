066fbb35e8f19540dc12a1618bb180f877a5e10d
Leon Mwandiringa <leonmwandiringa@gmail.com>
2024-10-05 18:02:28 +0200

    exported capacity provider nane

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "cluster_arn" {
   description = "ECS cluster arn"
   value       = aws_ecs_cluster.default.arn
 }
+
+output "ec2_capacity_provider_name" {
+  description = "capacity provider name"
+  value       = var.auto_scaling_group_arn == null ? "" : aws_ecs_capacity_provider.default[0].name
+}
